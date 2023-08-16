a7c91d54a67b034911ae0fb2d42f8f8c9b7675e4
andantonyan <andantonyan@gmail.com>
2023-08-16 23:56:31 +0400

    Add prompt examples and update dependencies
    Introduced examples of using various prompt selectors in src/prompt-example-selectors.js, src/prompt-similarity.js, and src/prompt-selectors.js. These new examples show how to format prompts using data or other selectors.

---
+++ src/prompt-selectors.js
@@ -0,0 +1,43 @@
+import { OpenAI } from 'langchain/llms/openai';
+import {
+  ChatPromptTemplate,
+  ConditionalPromptSelector,
+  HumanMessagePromptTemplate,
+  isChatModel,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from 'langchain/prompts';
+
+import './env.js';
+import { LLMChain, StuffDocumentsChain } from "langchain/chains";
+
+const openAI = new OpenAI({});
+const DEFAULT_QA_PROMPT = PromptTemplate.fromTemplate(
+  'Translate this sentence from English to French. I love programming.'
+);
+const CHAT_PROMPT = ChatPromptTemplate.fromPromptMessages([
+  SystemMessagePromptTemplate.fromTemplate(
+    'You are a helpful assistant that translates {input_language} to {output_language}.'
+  ),
+  HumanMessagePromptTemplate.fromTemplate('{text}'),
+]);
+
+const QA_PROMPT_SELECTOR = new ConditionalPromptSelector(DEFAULT_QA_PROMPT, [[isChatModel, CHAT_PROMPT]]);
+
+async function generateFormattedPrompt(llm) {
+  const promptTemplate = await QA_PROMPT_SELECTOR.getPrompt(llm);
+  const prompt = await promptTemplate.format({
+    text: 'I love programming.',
+    input_language: 'English',
+    output_language: 'French',
+  });
+  console.log(prompt);
+}
+
+await generateFormattedPrompt(openAI);
+
+function loadQAStuffChain(llm, params = {}) {
+  const { prompt = QA_PROMPT_SELECTOR.getPrompt(llm) } = params;
+  const llmChain = new LLMChain({ prompt, llm });
+  return new StuffDocumentsChain({ llmChain });
+}
