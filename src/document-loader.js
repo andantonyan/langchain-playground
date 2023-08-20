6824db67f1954e0ce45225024a8ec2fdbfa83633
andantonyan <andantonyan@gmail.com>
2023-08-20 19:39:15 +0400

    Add new example assets and create document-loader
    This commit adds several new example files in various formats (csv, json, jsonl, txt) and an new script named 'document-loader.js'. The script uses multiple loader types from the 'langchain' library to load example files. It demonstrates how to use different loader types for different file formats and a combined directory loader which uses appropriate loader based on file extension.

---
+++ src/document-loader.js
@@ -0,0 +1,70 @@
+import { Document } from 'langchain/document';
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import {
+  JSONLoader,
+  JSONLinesLoader,
+} from "langchain/document_loaders/fs/json";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+async function load() {
+  const loader = new TextLoader('assets/example.txt');
+  const docs = await loader.load();
+
+  console.log(docs);
+}
+
+async function create() {
+  const doc = new Document({
+    pageContent: 'Hello, World!',
+    metadata: {
+      source: '1'
+    }
+  });
+
+  console.log(doc);
+}
+
+
+async function csv() {
+  const loader = new CSVLoader('assets/example.csv');
+
+  const docs = await loader.load();
+
+  console.log(docs);
+}
+
+async function extractSingleColumn() {
+  const loader = new CSVLoader(
+    'assets/example.csv',
+    'text'
+  );
+
+  const docs = await loader.load();
+
+  console.log(docs);
+}
+
+async function fileDirectory() {
+
+  const loader = new DirectoryLoader(
+    "assets",
+    {
+      ".json": (path) => new JSONLoader(path, "/texts"),
+      ".jsonl": (path) => new JSONLinesLoader(path, "/name"),
+      ".txt": (path) => new TextLoader(path),
+      ".csv": (path) => new CSVLoader(path, "text"),
+    }
+  );
+  const docs = await loader.load();
+  console.log(docs);
+}
+
+async function pdf() {
+  const loader = new PDFLoader('assets/example.pdf');
+
+  const docs = await loader.load();
+
+  console.log(docs);
+}
